17fd2ddd4b49361187a516456c0634786de886c9
subha <s.jha@wizcondigital.com>
2022-02-07 21:25:42 +0530

    protfoliyo galary added

---
+++ src/Components/portfolio/protfolioGalary/photos.js
@@ -0,0 +1,65 @@
+export const photos = [
+    {
+      src: "assets/gallery-5.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "assets/gallery-7.jpg",
+      width: 2,
+      height: 1
+    },
+    {
+      src: "assets/gallery-11.jpg",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "assets/gallery-6.jpg",
+      width: 2,
+      height: 3
+    },
+    {
+      src: "assets/gallery-8.jpg",
+      width: 3,
+      height: 2
+    },
+    {
+      src: "assets/gallery-9.jpg",
+      width: 3,
+      height: 2
+    },
+    {
+        src: "assets/gallery-2.jpg",
+        width: 3,
+        height: 4
+      },
+      
+      {
+        src: "assets/gallery-1.jpg",
+        width: 3,
+        height: 4
+      },
+      {
+        src: "assets/gallery-4.jpg",
+        width: 2,
+        height: 1
+      },
+      {
+        src: "assets/gallery-10.jpg",
+        width: 2,
+        height: 3
+      },
+      {
+        src: "assets/gallery-12.jpg",
+        width: 3,
+        height: 2
+      },
+      {
+        src: "assets/gallery-3.jpg",
+        width: 3,
+        height: 2
+      }
+   
+  ];
+  
