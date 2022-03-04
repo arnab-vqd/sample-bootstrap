113de20f0ef3e26de12bbdb089215da32d379971
subha <s.jha@wizcondigital.com>
2022-03-04 12:47:19 +0530

    updated code

--- src/Components/portfolio/protfolioGalary/photos.js
+++ src/Components/portfolio/protfolioGalary/photos.js
@@ -1,62 +1,98 @@
 export const photos = [
     {
+      id:1,
+      name:'Sweet Cup',
       src: "assets/gallery-5.jpg",
+      category:["Brand","Graphics"],
       width: 3,
       height: 4
     },
     {
+      id :2,
+      name:'Minimal Things',
       src: "assets/gallery-7.jpg",
+      category:["Design","Brand","Graphics"],
       width: 2,
       height: 1
     },
     {
+      id:3,
+      name:'Semantic Collection',
       src: "assets/gallery-11.jpg",
+      category:["Digita","Design","Graphics"],
       width: 3,
       height: 4
     },
     {
+      id:4,
+      name:"Wall Clock",
       src: "assets/gallery-6.jpg",
+      category:["Brand","Graphics"],
       width: 2,
       height: 3
     },
     {
+      id:5,
+      name:"Reading Content",
       src: "assets/gallery-8.jpg",
+      category:["Design","Graphics"],
       width: 3,
       height: 2
     },
     {
+      id:6,
+      name:'Great Objects',
       src: "assets/gallery-9.jpg",
+      category:["Design","Brand"],
       width: 3,
       height: 2
     },
     {
+        id:7,
+        name:"Creative Portfolio",
         src: "assets/gallery-2.jpg",
+        category:["Design","Brand"],
         width: 3,
         height: 4
       },
       
       {
+        id:9,
+        name:"Creative Portfolio",
         src: "assets/gallery-1.jpg",
+        category:["Design","Digita","Graphics"],
         width: 3,
         height: 4
       },
       {
+        id:8,
+        name:"Creative Portfolio",
         src: "assets/gallery-4.jpg",
+        category:["Design","Graphics","Brand"],
         width: 2,
         height: 1
       },
       {
+        id:10,
+        name:"Creative Portfolio",
         src: "assets/gallery-10.jpg",
+        category:"Graphics",
         width: 2,
         height: 3
       },
       {
+        id:11,
+        name:"Creative Portfolio",
         src: "assets/gallery-12.jpg",
+        category:["Brand","Graphics"],
         width: 3,
         height: 2
       },
       {
+        id:12,
+        name:"Creative Portfolio",
         src: "assets/gallery-3.jpg",
+        category:["Digita","Graphics","Design","Brand"],
         width: 3,
         height: 2
       }
